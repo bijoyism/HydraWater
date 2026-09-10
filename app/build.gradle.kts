1b36f4c303936fb1f6633067b77f827c73b6e12b
bijoyism <alsoknownasbijoy@gmail.com>
2026-09-10 12:52:25 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,28 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+android {
+    namespace = "com.jo.hydrawater"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.jo.hydrawater"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 2
+        versionName = "1.0"
+    }
+}
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation("androidx.activity:activity-compose:1.10.0")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.core:core-ktx:1.15.0")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
 
