23a940404af14a642adbd64543e915df34217f43
bijoyism <alsoknownasbijoy@gmail.com>
2026-09-10 13:14:59 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,14 @@ android {
         versionCode = 2
         versionName = "1.0"
     }
+compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+}
 }
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
